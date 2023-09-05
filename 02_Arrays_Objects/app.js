4ac5b5ac218bf6b5d1c60cdf2f6cfc171175c745
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2023-09-04 22:09:21 -0400

    JS one step
    Done!

---
+++ 02_Arrays_Objects/app.js
@@ -0,0 +1,24 @@
+let topSongs = [
+    'First Time Ever I Saw Your Face',
+    'God Only Knows',
+    'A Day In The Life',
+    'Life On Mars'
+];
+
+topSongs.push('Fortunate Son'); /*PUSH - add to the end*/
+
+topSongs.pop(); /*POP - remove an item from the end*/
+
+let topCars = [
+    'Ferrari California',
+    'Lamborghini Gallardo',
+    'Pagani Zonda'
+];
+
+topCars.unshift('Maseratti'); /*UNSHIFT add an item at the begin*/
+
+topCars.shift(); /*SHIFT remove an item from the begin*/
+
+
+
+
